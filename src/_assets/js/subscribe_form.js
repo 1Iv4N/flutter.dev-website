ff1f410e93d19a440bdfdadc3c482c6301412a9a
Juan Gomez <juancamilog@google.com>
2019-10-08 19:40:36 -0500

    Add validation form and update form desktop sizes.

--- src/_assets/js/subscribe_form.js
+++ src/_assets/js/subscribe_form.js
@@ -5,13 +5,21 @@ const successMessage = document.querySelector('.success-message');
 const requiredFields = Array.from(subscribeForm.querySelectorAll("[required]"));
 const heading = document.querySelector('.heading');
 const subheading = document.querySelector('.subheading');
+const formSubmitButton = document.querySelector('.form-submit-button');
 // Ajax elements
 const url = 'https://services.google.com/fb/submissions/flutter-updates/';
 const request = new XMLHttpRequest();
 let response = null;
 
-document.querySelector('.form-submit-button').addEventListener('click', () => {
+// Add change eventListener to required fields
+requiredFields.forEach(item => item.addEventListener('change', submitButtonHandler));
+
+// Validation before submit
+formSubmitButton.addEventListener('click', () => {
     const checkFormValidity = requiredFields.every(item => {
+        if (!item.reportValidity()) {
+            item.classList.add('invalid-field');
+        }
         return item.reportValidity() === true;
     });
 
@@ -33,6 +41,7 @@ document.querySelector('.form-submit-button').addEventListener('click', () => {
     request.send(new FormData(subscribeForm));
 });
 
+//
 function successHandler() {
     if (response === 'accepted') {
         const errorMessageClass = errorMessage.classList;
@@ -47,5 +56,21 @@ function successHandler() {
         }
     } else {
         errorMessage.classList.add('d-block');
+    }
+}
+
+// Validation when required inputs change
+function submitButtonHandler () {
+    const requiredFieldsChecked = requiredFields.every(item => item.checkValidity() === true);
+    const submitButtonClass = formSubmitButton.classList;
+
+    if (requiredFieldsChecked) {
+        submitButtonClass.add('btn-primary');
+        submitButtonClass.remove('form-submit-button__disabled');
+        formSubmitButton.disabled = false;
+    } else if (submitButtonClass.contains('btn-primary')) {
+        submitButtonClass.remove('btn-primary');
+        submitButtonClass.add('form-submit-button__disabled');
+        formSubmitButton.disabled = true;
     }
 }
